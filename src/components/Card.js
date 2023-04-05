17252f1472466fbc68ee1c747bb7b51b7a2ffdb7
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-04-05 16:09:52 +0900

    map

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ export default function Card(props){
         <img src={`https://codingapple1.github.io/shop/shoes${props.i +1}.jpg`} width="80% " />
         <h4>{props.shoes.title}</h4>
         <p>{props.shoes.price}</p>
+        
       </div>
     </>
   );
